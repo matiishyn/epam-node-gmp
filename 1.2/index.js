465995d079558453f6fb90197bc9c99cccf9e237
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2020-03-30 22:52:12 +0300

    code clean up

--- 1.2/index.js
+++ 1.2/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
-const parseFile = require('./file');
+const parseFile = require('./lib/file');
 
 const inputFile = process.env.INPUT_FILEPATH || './files/input.csv';
 const outputFile = process.env.OUTPUT_FILEPATH || './files/output.txt';
